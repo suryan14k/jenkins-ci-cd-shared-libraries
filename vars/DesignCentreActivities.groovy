b324352a1a46154977b029a9c70168ab90935013
suryank <surya14k@gmail.com>
2021-08-31 09:39:58 +0530

    new file

---
+++ vars/DesignCentreActivities.groovy
@@ -0,0 +1,14 @@
+import org.ci.api.clients.*
+
+def hello(){
+    return "hello"
+}
+def getAnypointToken(props){
+    return getAnypointToken(props)
+}
+def getProjectID(props, token, projectName){
+    return getProjectID(props, token, projectName)
+}
+def saveProjectFiles(props, token,projectId, branch, apiDirPath){
+    return saveProjectFiles(props, token,projectId, branch, apiDirPath)
+}
